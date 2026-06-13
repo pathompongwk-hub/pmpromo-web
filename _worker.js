b5014a02b52a45051f9f0b43b8322cb1349415ac
pathompongwk-hub <pathompong.wk@gmail.com>
2026-06-13 16:58:56 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,27 +1,50 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    
+
     if (url.pathname === "/auth") {
-      const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo`;
+      const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user`;
       return Response.redirect(redirectUrl, 302);
     }
-    
+
     if (url.pathname === "/callback") {
       const code = url.searchParams.get("code");
-      const response = await fetch("https://github.com/login/oauth/access_token", {
+      if (!code) return new Response("Missing code", { status: 400 });
+
+      const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
         method: "POST",
-        headers: { "Content-Type": "application/json", "Accept": "application/json" },
-        body: JSON.stringify({ client_id: env.GITHUB_CLIENT_ID, client_secret: env.GITHUB_CLIENT_SECRET, code })
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          client_id: env.GITHUB_CLIENT_ID,
+          client_secret: env.GITHUB_CLIENT_SECRET,
+          code
+        })
       });
-      const data = await response.json();
-      const token = data.access_token;
+
+      const tokenData = await tokenRes.json();
+      const token = tokenData.access_token;
+
       const html = `<!DOCTYPE html><html><body><script>
-        window.opener.postMessage('authorization:github:success:{"token":"${token}","provider":"github"}', '*');
+        (function() {
+          function receiveMessage(e) {
+            window.opener.postMessage(
+              'authorization:github:success:{"token":"${token}","provider":"github"}',
+              e.origin
+            );
+          }
+          window.addEventListener("message", receiveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        })();
       </script></body></html>`;
-      return new Response(html, { headers: { "Content-Type": "text/html" } });
+
+      return new Response(html, {
+        headers: { "Content-Type": "text/html" }
+      });
     }
-    
+
     return env.ASSETS.fetch(request);
   }
 }
